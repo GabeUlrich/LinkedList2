71c17841c4a382a2154b7901db70ae29cc9e7c76
Gabriel Ulrich <ulrichgw06@mounties.mansfield.edu>
2017-04-21 07:39:15 -0500

    LinkedList2

---
+++ src/linkedlist2/Link.java
@@ -0,0 +1,23 @@
+package linkedlist2;
+
+/**
+ * Represents a single link object.
+ *
+ * @author Gabe
+ */
+public class Link {
+
+    public Friend data; // can replace this with any kind of object
+    public Link next;
+    public Link previous;
+
+    public Link(Friend data) {
+        this.data = data;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public String toString() {
+        return data.toString();
+    }
+}
